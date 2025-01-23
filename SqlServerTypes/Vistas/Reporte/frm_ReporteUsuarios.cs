06a67cd333ae58a2cb08843b62e38cae3d19c2ef
Doris Dayana Lopez Carvajal <daya278@outlook.es>
2025-01-23 11:42:41 -0500

    Agregar archivos de proyecto.

---
+++ SqlServerTypes/Vistas/Reporte/frm_ReporteUsuarios.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Semana_6___Tarea_1.SqlServerTypes.Vistas.Reporte
+{
+    public partial class frm_ReporteUsuarios : Form
+    {
+        public frm_ReporteUsuarios()
+        {
+            InitializeComponent();
+        }
+
+        private void frm_ReporteUsuarios_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
